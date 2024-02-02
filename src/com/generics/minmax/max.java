178f7a2240ede42db2c610c2f785732de7312ada
PrakashML <prakashkrishnan08@gmail.com>
2024-02-02 12:02:15 +0530

    find the maximum for 3 float type numbers

--- src/com/generics/minmax/max.java
+++ src/com/generics/minmax/max.java
@@ -1,35 +1,35 @@
 package com.generics.minmax;
 
 public class max {
-    Integer n1;
-    Integer n2;
-    Integer n3;
-    public max(Integer n1,Integer n2,Integer n3){
-        this.n1 = n1;
-        this.n2 = n2;
-        this.n3 = n3;
-    }
 
     public static void main(String[] args) {
-        max tc1 = new max(50,45,30);
         findmaxi(50,45,30);
-        max tc2 = new max(45,50,30);
         findmaxi(45,50,30);
-        max tc3 = new max(30,45,50);
         findmaxi(30,45,50);
-
+        findmaxf(50.2f,45.6f,30.9f);
+        findmaxf(45.6f,50.2f,30.9f);
+        findmaxf(45.6f,30.9f,50.2f);
     }
     private static void findmaxi(int n1,int n2,int n3) {
         Integer temp = n1;
-        if(temp.compareTo(n2)>0){
-            temp = n1;
-        }
-        else if(temp.compareTo(n3)>0){
+        if(temp.compareTo(n2)<0){
             temp = n2;
         }
-        else{
+        if(temp.compareTo(n3)<0){
             temp = n3;
         }
+        System.out.println(temp);
+    }
+
+    private static void findmaxf(Float n1, Float n2,Float n3){
+        Float temp = n1;
+        if(temp.compareTo(n2)<0){
+            temp = n2;
+        }
+        if(temp.compareTo(n3)<0){
+            temp = n3;
+        }
+
         System.out.println(temp);
     }
 }
